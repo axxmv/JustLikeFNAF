811320390fe15af10975e866beedaeb3b9c23897
axxmv <andreavcruzm@gmail.com>
2026-08-30 12:28:18 -0500

    initial commit

---
+++ Parser.py
@@ -0,0 +1,6 @@
+#The parser will take raw log data and structure it 
+#for the Detection engine. 
+#The parser will also update the events table to document
+#all events regardless of if they set off an alert
+
+
